9d7d1b4ba676e9b939d32a6835defaa5a878625d
Sander Boer <sanderboer@mauc.nl>
2024-08-10 21:13:05 +0200

    raylib shared

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
-
 #include <iostream>
-
-// Include OpenCascade headers
 #include <BRepPrimAPI_MakeBox.hxx>
 #include <TopoDS_Shape.hxx>
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
 int main() {
-    // Create a box using OpenCascade
     TopoDS_Shape box = BRepPrimAPI_MakeBox(100, 50, 25).Shape();
 
     std::cout << "Box created successfully!" << std::endl;
+
+
+    // Initialization
+    InitWindow(800, 450, "Hello Raylib + Raygui");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Draw Hello World using Raygui
+        GuiLabel((Rectangle){350, 220, 100, 20}, "Box created successfully!");
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
